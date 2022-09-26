3fb91c3c29c6c2d748b377b74710a9ab34296848
DAKingFish <891248330@qq.com>
2022-09-26 19:01:52 +0800

    项目第一次提交

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.scss'
+//导入路由组件
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Login from './pages/Login'
+import Layout from './pages/Layout'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/home" component={Layout} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
