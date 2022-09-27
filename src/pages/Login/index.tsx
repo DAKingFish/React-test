1d135364b9ace2566216b75a0b6578ccb4d1758a
DAKingFish <891248330@qq.com>
2022-09-27 19:58:08 +0800

    提交错误代码

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,37 +1,51 @@
-const Login = () => {
+import { Layout } from 'antd'
+import React from 'react'
+import './index.scss'
+// 导入 antd 组件库的样式
+import 'antd/dist/antd.css'
+import backimg from '../../assets/1.png'
+import logo from '../../assets/2.png'
+const { Header, Sider, Content } = Layout
+
+const Login: React.FC = () => {
+  function changeLogin  = (e:any)=>{
+    
+  }
   return (
-    <div className="login">
-      <div className="login_left">
-        <div className="l_left_top">
-          <img src="" alt="logo" />
-        </div>
-        <div className="l_left_middle">
-          <img src="" alt="back-img" />
-        </div>
-      </div>
-      <div className="login_right">
-        <div className="go_main">
-          <a
-            href="https://www.xiaolanben.com/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            进入官网
-          </a>
-        </div>
-        <div className="login_body">
-          <div className="login_tab">欢迎使用小蓝本</div>
-          <div className="login_select"></div>
-          <div className="login_body1">
-            <div className="body1_top">
-              <div className="b1_top1">账号登录</div>
-              <div className="b1_top2">验证码登录</div>
-            </div>
+    <div>
+      <Layout className="login">
+        <Sider className="login_left" width="560">
+          <div className="l_left_top">
+            <img src={logo} alt="logo" />
           </div>
-          <div className="login_body2"></div>
-          <div className="login_body3"></div>
-        </div>
-      </div>
+          <div className="l_left_middle">
+            <img src={backimg} alt="backimg" />
+          </div>
+        </Sider>
+        <Layout className="login_right">
+          <Header className="go_main">
+            <a
+              href="https://www.xiaolanben.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              进入官网
+            </a>
+          </Header>
+          <Content className="login_body">
+            <div className="login_tab">欢迎使用小蓝本</div>
+            <div className="login_select"></div>
+            <div className="login_body1">
+              <div className="body1_top">
+                <div className="b1_top1" onClick={(e)=>changeLogin(this)}>账号登录</div>
+                <div className="b1_top2">验证码登录</div>
+              </div>
+            </div>
+            <div className="login_body2"></div>
+            <div className="login_body3"></div>
+          </Content>
+        </Layout>
+      </Layout>
     </div>
   )
 }
