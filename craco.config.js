4f5e4cd931ea941ce54c25342285d92adb8aace9
DAKingFish <891248330@qq.com>
2022-10-17 10:26:08 +0800

    删除代理配置

--- craco.config.js
+++ craco.config.js
@@ -5,16 +5,5 @@ module.exports = {
     alias: {
       '@': path.join(__dirname, 'src'), // 允许通过@符号来表示 src目录
     },
-    devServer: {
-      proxy: {
-        '/api': {
-          target: 'http://127.0.0.1:8080/', //需要代理的地址
-          changeOrigin: true,
-          pathRewrite: {
-            '^/api': '',
-          },
-        },
-      },
-    },
   },
 }
